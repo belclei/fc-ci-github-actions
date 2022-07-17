1bd4b9110096f4924bbc7fb7d2e9bb3647f176ac
belclei <belclei@gmail.com>
2022-07-16 22:06:27 -0300

    test: add kelvin convertion test

--- src/main.test.js
+++ src/main.test.js
@@ -9,6 +9,9 @@ test('0°C must be 273.15°K', () => {
 test('32°F must be 0°C', () => {
   expect(convert.fahrenheitToCelsius(32)).toBe(0)
 })
+test('273.15K must be 0°C', () => {
+  expect(convert.kelvinToCelsius(273.15)).toBe(0)
+})
 test('1GB must be 1073741824B', () => {
   expect(convert.gigabyteToByte(1)).toBe(1073741824)
 })
