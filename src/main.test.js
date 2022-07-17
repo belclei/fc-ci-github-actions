0d497282a648e2471ee2c0d77a4f8aa47aba1542
belclei <belclei@gmail.com>
2022-07-16 21:10:40 -0300

    test: add celsius to kelvin test

--- src/main.test.js
+++ src/main.test.js
@@ -3,6 +3,9 @@ const convert = require('./main')
 test('0°C must be 32°F', () => {
   expect(convert.celsiusToFahrenheit(0)).toBe(32)
 })
+test('0°C must be 273.15°K', () => {
+  expect(convert.celsiusToKelvin(0)).toBe(273.15)
+})
 test('32°F must be 0°C', () => {
   expect(convert.fahrenheitToCelsius(32)).toBe(0)
 })
